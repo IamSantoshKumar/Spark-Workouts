739bf4acb4a26ad4ecb1fbf89818c00efecb1552
Santhoshkumar V <shanvsanthosh@gmail.com>
2019-05-23 22:55:01 +0530

    spark core
    spark core

---
+++ Lab07.scala
@@ -0,0 +1,22 @@
+package com.inceptez.spark.core
+/*
+
+hdpcd/dir7A/InceptezA.txt
+Inceptez.com QuickTechie.com Training4Exam.com
+
+hdpcd/dir7B/InceptezB.txt
+Hadoop Spark Scala Python Java Cloud Science
+
+hdpcd/dir7C/InceptezC.txt
+India USA UK Canada Australia
+
+Accomplish the followings:-
+
+1. Load the data given in all 3 directories
+2. Once the data is loaded filter the words which contains "Hadoop"
+#3. Now persist the data in memory
+
+ * */
+object Lab07 {
+  
+}
